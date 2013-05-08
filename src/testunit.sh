9e88c7b85819640dab4041613db93b998c3861a6
Damir Sh. Yakupov <yds@bk.ru>
2013-05-08 12:16:00 +0000

    testunit.sh - changed the output to the log

--- src/testunit.sh
+++ src/testunit.sh
@@ -13,8 +13,8 @@
 #   Здесь указываются модули, код которых используется данной библиотекой
 # SOURCE
 : ${_bashlyk_pathLib:=/usr/share/bashlyk}
+: ${_bashlyk_pathLog:=/tmp}
 : ${_bashlyk_TestUnit_iCount=0}
-: ${_bashlyk_TestUnit_fnLog=/tmp/testunit.log}
 : ${_bashlyk_TestUnit_fnTmp=$(mktemp 2>/dev/null || tempfile)}
 #******
 #****f* bashlyk/testunit/udfTestUnitMsg
@@ -33,13 +33,15 @@ udfTestUnitMsg() {
  esac
  [ "$rc0" = "$rc1" ] && rc2='.' || rc2='?'
  echo -n $rc2
- if [ "$rc2" = "?" ]; then
-  {
+ {
+  if [ "$rc2" = "?" ]; then
    _bashlyk_TestUnit_iCount=$((_bashlyk_TestUnit_iCount+1))
-   echo "? rc: [ $rc0 != $rc1 ] - ${BASH_SOURCE[1]} (line ${BASH_LINENO[0]}):"
+   echo "--[?]: status $rc0 ( must have $rc1 ) - ${BASH_SOURCE[1]} (line ${BASH_LINENO[0]}):"
    head -n ${BASH_LINENO[0]} ${BASH_SOURCE[1]} | tail -n 1
-  } >> $_bashlyk_TestUnit_fnLog
- fi
+  else
+   echo "-- ok"
+  fi
+ } >> $_bashlyk_TestUnit_fnLog
  return 0
 } 
 #******
@@ -52,13 +54,14 @@ udfMain() {
  local a s fn
  fn=${_bashlyk_pathLib}/lib${1}.sh
  [ -s $fn ] && . $fn || return 254
- _bashlyk_TestUnit_fnLog=/tmp/${1}.testunit.log
+ _bashlyk_TestUnit_fnLog=${_bashlyk_pathLog}/${1}.testunit.log
+
  a=$(eval echo '$_bashlyk_aExport_'"${1}")
  {
   for s in $a; do
-   echo 'echo "-- testing '"${s}"':" >>$_bashlyk_TestUnit_fnLog'
+   echo 'echo "-- '"${s}"' testing start:" >>$_bashlyk_TestUnit_fnLog'
    grep "^#.*#.${s}" $fn | grep -w "#.${s}" | sed -e "s/^#//" | sed -e "s/##${s}/>>\$_bashlyk_TestUnit_fnLog 2>\&1/" -e "s/#-${s}//"  -e "s/\? \(.*\)/; udfTestUnitMsg \1/" -e "s/[ ]\+$//"
-   echo 'echo "--" >>$_bashlyk_TestUnit_fnLog'
+   echo 'echo "-- '"${s}"' testing  done." >>$_bashlyk_TestUnit_fnLog'
    done
  } >> $_bashlyk_TestUnit_fnTmp
  echo "testunit for $fn library" > $_bashlyk_TestUnit_fnLog
@@ -69,7 +72,7 @@ udfMain() {
   rm -f $_bashlyk_TestUnit_fnTmp
  else
   echo " fail.."
-  echo "found $_bashlyk_TestUnit_iCount errors. See \"[?] - test unit error -\" lines from $_bashlyk_TestUnit_fnLog"
+  echo "found $_bashlyk_TestUnit_iCount errors. See \"[?]: status\" lines from $_bashlyk_TestUnit_fnLog"
  fi
 }
 #******
