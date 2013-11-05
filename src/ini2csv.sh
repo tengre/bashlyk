a712ac3a12c40ec9141ba930c84c511acd400aa4
Damir Sh. Yakupov <yds@bk.ru>
2013-11-05 20:12:16 +0000

    ini2csv.sh - fixed udfIni2Csv via test unit

---
+++ src/ini2csv.sh
@@ -0,0 +1,17 @@
+csv=';[];a=b;[test];sTxt=foo;b=false;_bashlyk_ini_test_autoKey_0="iXo Xo=19";iYo=80;_bashlyk_ini_test_autoKey_1="simple line";;;[];a=c;[test];sTxt="foo bar";b=true;iXo=1921;iYo=1080;;'
+
+aTag=$(echo $csv | tr ';' '\n' | grep -oE '\[.*\]' | sort | uniq | tr -d '[]' | tr '\n' ' ')
+sR=''
+for s in "" $aTag; do
+ sT=''
+ sS='\['${s}'\]'
+ while [ true ]; do
+  [ -n "$(echo $csv | grep -oE $sS)" ] || break
+  sF=$(echo "${csv#*$sS;}" | cut -f1 -d'[')
+  csv=$(echo ${csv/$sS;$sF/})
+  sT+=";"${sF}
+ done
+ sR+="[${s}];${sT};"
+done
+echo ${sR} | sed -e "s/;\+/;/g"
+
