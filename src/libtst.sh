10c6f4dc7221de8bd93c4638b46777d2e5c388fa
Damir Sh. Yakupov <yds@bk.ru>
2016-09-19 21:36:13 +0400

    libtst.sh - testing new version of udfMakeTemp

--- src/libtst.sh
+++ src/libtst.sh
@@ -1,5 +1,5 @@
 #
-# $Id: libtst.sh 545 2016-08-22 02:05:53+04:00 toor $
+# $Id: libtst.sh 552 2016-09-19 21:35:49+04:00 toor $
 #
 #****h* BASHLYK/libtst
 #  DESCRIPTION
@@ -36,6 +36,226 @@
 : ${_bashlyk_envXSession:=}
 : ${_bashlyk_aRequiredCmd_msg:="[ "}
 : ${_bashlyk_aExport_msg:="udfTest"}
+#******
+#****f* libtst/udfMakeTemp
+#  SYNOPSIS
+#    udfMakeTemp [varname] options...
+#  DESCRIPTION
+#    Создание временного файла или каталога
+#  INPUTS
+#    varname=[<varid>] - идентификатор переменной для возврата результата, если
+#                        аргумент не именной, то должен быть всегда первый
+#    path=<path>       - каталог, в котором будут создаваться временные объекты
+#    prefix=<prefix>   - префикс имени временного объекта
+#    suffix=<suffix>   - суффикс имени временного объекта
+#    mode=<mode>       - права на временный объект
+#    owner=<owner>     - владелец временного объекта
+#    group=<group>     - группа временного объекта
+#    type=file|dir     - тип объекта: файл (по умолчанию) или каталог
+#    keep=true|false   - удалять/не удалять временные объекты после завершения
+#                        сценария. По умолчанию, удаляется, если имя временного
+#                        объекта передается аргументу-переменной, если оно
+#                        выдается на stdout, то не удаляется
+#  OUTPUT
+#    вывод происходит если нет аргументов или отсутствует именной аргумент
+#    varname, если временный объект не создан, то ничего не выдается
+#
+#  RETURN VALUE
+#    0                        - выполнено успешно
+#    iErrorNotExistNotCreated - временный объект файловой системы не создан
+#    iErrorNonValidVariable   - аргумент <varname> не является валидным
+#                               идентификатором переменной
+#
+#  EXAMPLE
+#    local foTemp
+#    udfMakeTemp -v foTemp path=$HOME prefix=pre. suffix=.suf
+#    ls $foTemp >| grep -w "$HOME/pre\.........\.suf"                           #? true
+#    udfMakeTemp -v foTemp type=dir mode=0751
+#    ls -ld $foTemp >| grep "^drwxr-x--x.*${foTemp}$"                           #? true
+#    foTemp=$(udfMakeTemp prefix=pre. suffix=.suf)
+#    ls $foTemp >| grep "pre\.........\.suf$"                                   #? true
+#    rm -f $foTemp
+#    $(udfMakeTemp -v foTemp prefix=pre. suffix=.suf)
+#    test -f $foTemp                                                            #? false
+#    unset foTemp
+#    foTemp=$(udfMakeTemp)                                                      #? true
+#    test -f $foTemp                                                            #? true
+#    rm -f $foTemp
+#    udfMakeTemp 2t                                                             #? ${_bashlyk_iErrorNonValidVariable}
+#    udfMakeTemp path=/proc                                                     #? ${_bashlyk_iErrorNotExistNotCreated}
+#  SOURCE
+udfMakeTemp() {
+
+	if [[ "$1" == "-v" ]]; then
+
+		shift
+
+		udfIsValidVariable "$1" || eval $( udfOnError2 InvalidArgument "$2" )
+
+		eval 'export $1="$( udfMakeTemp $* )"'
+
+		[[ -n ${!1} ]] || eval $( udfOnError2 iErrorEmptyResult "$2" )
+
+		if [[ $* =~ keep=false ]]; then
+
+			case $* in
+
+				*type=d*) udfAddPath2Clean ${!1};;
+				*type=f*) udfAddFile2Clean ${!1};;
+				*type=p*) udfAddFile2Clean ${!1};;
+
+			esac
+
+		fi
+
+		return $?
+
+	fi
+
+	local foResult optDir s bPipe sVar sGroup sCreateMode path sUser sPrefix sSuffix rc octMode IFS=$' \t\n'
+ #
+ sCreateMode=direct
+ #
+ for s in $*; do
+  case "$s" in
+     path=*) path=${s#*=};;
+   prefix=*) sPrefix=${s#*=};;
+   suffix=*) sSuffix=${s#*=};;
+     mode=*) octMode=${s#*=};;
+    type=d*) optDir='-d';;
+    type=f*) optDir='';;
+    type=p*) bPipe=1;;
+     user=*) sUser=${s#*=};;
+    group=*) sGroup=${s#*=};;
+    keep=t*) bNoKeep=false;;
+    keep=f*) bNoKeep=true;;
+  varname=*) sVar=${s#*=};;
+          *)
+            sVar="$1"
+            udfIsNumber "$2"
+            rc=$?
+            if [[ -z "$3" && -n "$2" && $rc -eq 0 ]]; then
+             # oldstyle
+             octMode="$2"
+             sVar=''
+             sPrefix="$1"
+            fi
+          ;;
+  esac
+ done
+
+	if   [[ -f "$(which mktemp)" ]]; then
+
+		sCreateMode=mktemp
+
+	elif [[ -f "$(which tempfile)" ]]; then
+
+		[[ -z "$optDir" ]] && sCreateMode=tempfile || sCreateMode=direct
+
+	fi
+
+	case "$sCreateMode" in
+
+	direct)
+
+		if [[ -n "$path" ]]; then
+
+			s="${path}/"
+
+		else
+
+			if [[ -z $bPipe ]]; then
+
+				s="/tmp/"
+			else
+
+				mkdir -p $( _ pathRun )
+				s=$( _ pathRun )
+
+			fi
+
+		fi
+
+		s+="${sPrefix}${$}${sSuffix}"
+
+		[[ -n "$optDir" ]] && mkdir -p $s || touch $s
+
+	;;
+
+	mktemp)
+
+		if [[ -n "$path" ]]; then
+
+			mkdir -p ${path}
+			path="--tmpdir=${path}"
+
+		else
+
+			if [[ -z $bPipe ]]; then
+
+				path="--tmpdir=/tmp"
+
+			else
+
+				mkdir -p $( _ pathRun )
+				path="--tmpdir=$( _ pathRun )"
+
+			fi
+
+		fi
+
+		if [[ -n "$sPrefix" ]]; then
+
+			sPrefix=$(echo $sPrefix | tr -d '/')
+
+		fi
+
+		if [[ -n "${sSuffix}" ]]; then
+
+			sSuffix="--suffix=$(echo ${sSuffix} | tr -d '/')"
+
+		fi
+
+		s=$(mktemp $path $optDir ${sSuffix} "${sPrefix}XXXXXXXX")
+
+	;;
+
+	tempfile)
+
+		[[ -n "$sPrefix" ]] && sPrefix="-p $sPrefix"
+		[[ -n "$sSuffix" ]] && sSuffix="-s $sSuffix"
+		s=$(tempfile $optDir $sPrefix $sSuffix)
+
+	;;
+
+	*)
+		## не достижимое состояние
+		eval $(udfOnError return iErrorUnexpected $sCreateMode)
+	;;
+
+	esac
+
+	if [[ -n $bPipe ]]; then
+
+		rm -f  $s
+		mkfifo $s
+
+	fi
+
+	[[ -n "$octMode" ]] && chmod $octMode $s
+
+ ## TODO обработка ошибок
+ [[ -n "$sUser"  ]] && chown $sUser  $s
+ [[ -n "$sGroup" ]] && chgrp $sGroup $s
+
+ if ! [[ -f "$s" || -p "$s" || -d "$s" ]]; then
+  eval $(udfOnError return iErrorNotExistNotCreated $s)
+ fi
+
+ echo $s
+
+ return 0
+}
 #******
 #****f* libtst/udfTest
 #  SYNOPSIS
