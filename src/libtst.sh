e477521c1470e8fca82eb0eaf785245649ee4480
Damir Sh. Yakupov <yds@bk.ru>
2017-04-24 20:47:41 +0400

    tst - improved 'on error', added 'throw+' handler
    liberr.sh:
    - added iErrorNotDetected state for success code
    
    libtst.sh:
    - err::handler renamed to err::eval[uate]:
      * updated <action> description of the robodoc section
      * add warns for the unexpected error code
      * improved arguments handling
    - added err::{
        CommandNotFound,NoSuchFileOrDir,InvalidVariable,
        EmptyVariable,MissingArgument,EmptyResult
      } - advanced analogues of the handlers from the liberr
    - added err::handler - advanced analogue udfOn ... from liberr
    - added aliases for 'err::handler <action> ...':
              ECHO='err::handler echo'
              EXIT='err::handler exit'
              warn='err::handler warn'
             throw='err::handler throw'
            RETURN='err::handler return'
         echo+exit='err::handler echo+exit'
         warn+exit='err::handler warn+exit'
       echo+return='err::handler echo+return'
       warn+return='err::handler warn+return'

--- src/libtst.sh
+++ src/libtst.sh
@@ -1,5 +1,5 @@
 #
-# $Id: libtst.sh 743 2017-04-24 00:11:10+04:00 toor $
+# $Id: libtst.sh 744 2017-04-24 17:27:08+04:00 toor $
 #
 #****h* BASHLYK/libtst
 #  DESCRIPTION
@@ -45,6 +45,17 @@
 declare -rg _bashlyk_externals_tst=""
 declare -rg _bashlyk_exports_tst="udfTest"
 #******
+shopt -s expand_aliases
+alias on='eval $( err::eval )'
+alias ECHO='err::handler echo'
+alias EXIT='err::handler exit'
+alias warn='err::handler warn'
+alias throw='err::handler throw'
+alias RETURN='err::handler return'
+alias echo+exit='err::handler echo+exit'
+alias warn+exit='err::handler warn+exit'
+alias echo+return='err::handler echo+return'
+alias warn+return='err::handler warn+return'
 #****f* libtst/udfTest
 #  SYNOPSIS
 #    udfTest args
@@ -153,9 +164,9 @@ __private() {
 
 }
 #******
-#****f* liberr/err::handler
+#****f* liberr/err::eval
 #  SYNOPSIS
-#    err::handler [<action>] [<state>] [<message>]
+#    err::eval [<action>] [<state>] [<message>]
 #  DESCRIPTION
 #    Flexible error handling. Processing is controlled by global variables or
 #    arguments, and consists in a warning message, the function returns, or the
@@ -164,24 +175,25 @@ __private() {
 #  INPUTS
 #    <action> - directly determines how the error handling. Possible actions:
 #
-#     echo     - just prepare a message from the string argument to STDOUT
-#     warn     - prepare a message from the string argument for transmission to
-#                the notification system
-#     return   - set return from the function. In the global context - the end
-#                of the script (exit)
-#     retecho  - the combined action of 'echo'+'return', however, if the code is
-#                not within the function, it is only the transfer of messages
-#                from a string of arguments to STDOUT
-#     retwarn  - the combined action of 'warn'+'return', however, if the code is
-#                not within the function, it is only the transfer of messages
-#                from a string of arguments to the notification system
-#     exit     - set unconditional completion of the script
-#     exitecho - the same as 'exit', but with the transfer of messages from a
-#                string of arguments to STDOUT
-#     exitwarn - the same as 'exitecho', but with the transfer of messages to
-#                the notification system
-#     throw    - the same as 'exitwarn', but with the transfer of messages and
-#                the call stack to the notification system
+#     echo        - just prepare a message from the string argument to STDERR
+#     warn        - prepare a message from the string argument for transmission
+#                   to the notification system
+#     return      - set return from the function. In the global context - the
+#                   end of the script (exit)
+#     echo+return - the combined action of 'echo'+'return', however, if the code
+#                   is not within the function, it is only the transfer of
+#                   messages from a string of arguments to STDERR
+#     warn+return - the combined action of 'warn'+'return', however, if the code
+#                   is not within the function, it is only the transfer of
+#                   messages from a string of arguments to the notification
+#                   system
+#     exit        - set unconditional completion of the script
+#     echo+exit   - the same as 'exit', but with the transfer of messages from a
+#                   string of arguments to STDERR
+#     warn+exit   - the same as 'echo+exit', but with the transfer of messages
+#                   to the notification system
+#     throw       - the same as 'warn+exit', but with the transfer of messages
+#                   and the call stack to the notification system
 #
 #    If an action is not specified, it uses stored in the global variable
 #    $_bashlyk_onError action. If it is not valid, then use action 'throw'
@@ -201,16 +213,18 @@ __private() {
 #  OUTPUT
 #    command line, which can be performed using the eval <...>
 #  EXAMPLE
-#    false || on error warn InvalidArgument message error                       #? true
+#    false || on error warn NoSuchFileOrDir /notexist                           #? true
 #  SOURCE
-err::handler() {
+err::eval() {
 
+  #local -a a
   local rc=$? re rs sAction=$_bashlyk_onError sMessage='' s IFS=$' \t\n'
   re='^(echo|exit|echo\+exit|warn\+exit|echo\+return|warn\+return|return|warn|throw)$'
 
   s="$( sed -n "${BASH_LINENO[0]}p" ${BASH_SOURCE[1]} )"
 
   s="${s##*on error }"; s="${s%%>*}"
+
   eval set -- "$s"
 
   [[ ${1,,} =~ $re ]] && sAction=${1,,} && shift
@@ -219,16 +233,16 @@ err::handler() {
 
   if [[ $rs == $_bashlyk_iErrorUnknown ]]; then
 
+    s="(bad error code applied, i try to use the previous..): "
+
     if [[ ${_bashlyk_hError[$rc]} ]]; then
 
       rs=$rc
-      s="${_bashlyk_hError[$rc]} - $* .. ($rc)"
+      s+="${_bashlyk_hError[$rc]}, detail: $* .. ($rc)"
 
     else
 
-      (( $rc == 0 )) && rc=$_bashlyk_iErrorUnexpected
-      s="$* .. ($rc)"
-      rs=$_bashlyk_iErrorUnexpected
+      s+="$* .. ($rc)"
 
     fi
 
@@ -238,11 +252,10 @@ err::handler() {
 
     if [[ ${_bashlyk_hError[$rs]} ]]; then
 
-      s="${_bashlyk_hError[$rs]} - $* .. ($rs)"
+      s="${_bashlyk_hError[$rs]}, detail: $* .. ($rs)"
 
     else
 
-      (( $rs == 0 )) && rs=$_bashlyk_iErrorUnexpected
       s="$* .. ($rs)"
 
     fi
@@ -280,5 +293,224 @@ err::handler() {
 
 }
 #******
-shopt -s expand_aliases
-alias on='eval $( err::handler )'
+#****f* liberr/err::CommandNotFound
+#  SYNOPSIS
+#    err::CommandNotFound <filename>
+#  DESCRIPTION
+#    return true if argument is not empty, exists and executable (## TODO test)
+#    designed to check the conditions in the function err::handler
+#  INPUTS
+#    filename - argument for executable file matching by searching the PATH
+#  RETURN VALUE
+#    CommandNotFound - no arguments, specified filename is nonexistent or not
+#                      executable
+#    0               - specified filename are found and executable
+#  EXAMPLE
+#    local cmdYes='sh' cmdNo1="bin_${RANDOM}" cmdNo2="bin_${RANDOM}"
+#    err::CommandNotFound                                                       #? $_bashlyk_iErrorCommandNotFound
+#    err::CommandNotFound $cmdNo1                                               #? $_bashlyk_iErrorCommandNotFound
+#    $( err::CommandNotFound $cmdNo2 || exit 123 )                              #? 123
+#    err::CommandNotFound $cmdYes                                               #? true
+#  SOURCE
+err::CommandNotFound() {
+
+  [[ $* ]] && hash "$*" 2>/dev/null || return $_bashlyk_iErrorCommandNotFound
+
+}
+#******
+#****f* liberr/err::NoSuchFileOrDir
+#  SYNOPSIS
+#    err::NoSuchFileOrDir <filename>
+#  DESCRIPTION
+#    return true if argument is non empty, exists, designed to check the
+#    conditions in the function err::handler
+#  ARGUMENTS
+#    <filename> - filesystem object for checking
+#  RETURN VALUE
+#    NoSuchFileOrDir - no arguments, specified filesystem object is nonexistent
+#    0               - specified filesystem object are found
+#  EXAMPLE
+#    local cmdYes='/bin/sh' cmdNo1="bin_${RANDOM}" cmdNo2="bin_${RANDOM}"
+#    err::NoSuchFileOrDir                                                       #? $_bashlyk_iErrorNoSuchFileOrDir
+#    err::NoSuchFileOrDir $cmdNo1                                               #? $_bashlyk_iErrorNoSuchFileOrDir
+#    $(err::NoSuchFileOrDir $cmdNo2 || exit 123)                                #? 123
+#    err::NoSuchFileOrDir $cmdYes                                               #? true
+#  SOURCE
+err::NoSuchFileOrDir() {
+
+  [[ $* && -e "$*" ]] || return $_bashlyk_iErrorNoSuchFileOrDir
+
+}
+#******
+#****f* liberr/err::InvalidVariable
+#  SYNOPSIS
+#    err::InvalidVariable <variable>
+#  DESCRIPTION
+#    return true if argument is non empty, valid variable, designed to check the
+#    conditions in the function err::handler
+#  INPUTS
+#    <variable> - expected variable name
+#  RETURN VALUE
+#    InvalidVariable - argument is empty or invalid variable
+#    0               - valid variable
+#  EXAMPLE
+#    err::InvalidVariable                                                       #? $_bashlyk_iErrorInvalidVariable
+#    err::InvalidVariable 1a                                                    #? $_bashlyk_iErrorInvalidVariable
+#    err::InvalidVariable a1                                                    #? true
+#    $(err::InvalidVariable 2b || exit 123)                                     #? 123
+#    $(err::InvalidVariable c3 && exit 123)                                     #? 123
+#  SOURCE
+err::InvalidVariable() {
+
+  [[ $* ]] && udfIsValidVariable "$*" || return $_bashlyk_iErrorInvalidVariable
+
+}
+#******
+#****f* liberr/err::EmptyVariable
+#  SYNOPSIS
+#    err::EmptyVariable <variable>
+#  DESCRIPTION
+#    return true if argument is non empty, valid variable
+#    designed to check the conditions in the function err::handler
+#  INPUTS
+#    variable - expected variable name
+#  RETURN VALUE
+#    EmptyVariable - argument is empty, invalid or empty variable
+#    0             - valid not empty variable
+#  EXAMPLE
+#    local a b="$RANDOM"
+#    err::EmptyVariable                                                         #? $_bashlyk_iErrorEmptyVariable
+#    err::EmptyVariable a                                                       #? $_bashlyk_iErrorEmptyVariable
+#    $(err::EmptyVariable a || exit 123)                                        #? 123
+#    $(err::EmptyVariable b && exit 123)                                        #? 123
+#    err::EmptyVariable b                                                       #? true
+#  SOURCE
+err::EmptyVariable() {
+
+  [[ $1 ]] && udfIsValidVariable "$1" && [[ ${!1} ]] || return $_bashlyk_iErrorEmptyVariable
+
+}
+#******
+#****f* liberr/err::MissingArgument
+#  SYNOPSIS
+#    err::MissingArgument <argument>
+#  DESCRIPTION
+#    return true if argument is not empty
+#    designed to check the conditions in the function err::handler
+#  INPUTS
+#    argument - one argument
+#  RETURN VALUE
+#    MissingArgument - argument is empty
+#    0               - non empty argument
+#  EXAMPLE
+#    local a b="$RANDOM"
+#    err::MissingArgument                                                       #? $_bashlyk_iErrorMissingArgument
+#    err::MissingArgument $a                                                    #? $_bashlyk_iErrorMissingArgument
+#    $(err::MissingArgument $a || exit 123)                                     #? 123
+#    $(err::MissingArgument $b && exit 123)                                     #? 123
+#    err::MissingArgument $b                                                    #? true
+#  SOURCE
+err::MissingArgument() {
+
+  [[ $* ]] || return $_bashlyk_iErrorMissingArgument
+
+}
+#******
+err::EmptyOrMissingArgument() { err::MissingArgument $*; }
+err::EmptyArgument()          { err::MissingArgument $*; }
+#****f* liberr/err::EmptyResult
+#  SYNOPSIS
+#    err::EmptyResult <argument>
+#  DESCRIPTION
+#    return true if argument is not empty
+#    designed to check the conditions in the function err::handler
+#  INPUTS
+#    <argument> - one argument
+#  RETURN VALUE
+#    EmptyResult - argument is empty
+#    0           - non empty argument
+#  EXAMPLE
+#    local a b="$RANDOM"
+#    err::EmptyResult                                                           #? $_bashlyk_iErrorEmptyResult
+#    err::EmptyResult $a                                                        #? $_bashlyk_iErrorEmptyResult
+#    $(err::EmptyResult $a || exit 123)                                         #? 123
+#    $(err::EmptyResult $b && exit 123)                                         #? 123
+#    err::EmptyResult $b                                                        #? true
+#  SOURCE
+err::EmptyResult() {
+
+  [[ $* ]] || return $_bashlyk_iErrorEmptyResult
+
+}
+#******
+#****f* liberr/err::handler
+#  SYNOPSIS
+#    err::handler <action> on <state> <argument>
+#  DESCRIPTION
+#    Flexible error handling. Processing is controlled by global variables or
+#    arguments, and consists in a warning message, the function returns, or the
+#    end of the script with a certain return code. Messages may include a stack
+#    trace and printed to stderr
+#  INPUTS
+#    <action> - directly determines how the error handling. Possible actions:
+#     echo        - just prepare a message from the string argument to STDERR
+#     warn        - prepare a message from the string argument for transmission
+#                   to the notification system
+#     return      - set return from the function. In the global context - the
+#                   end of the script (exit)
+#     echo+return - the combined action of 'echo'+'return', however, if the code
+#                   is not within the function, it is only the transfer of
+#                   messages from a string of arguments to STDERR
+#     warn+return - the combined action of 'warn'+'return', however, if the code
+#                   is not within the function, it is only the transfer of
+#                   messages from a string of arguments to the notification
+#                   system
+#     exit        - set unconditional completion of the script
+#     echo+exit   - the same as 'exit', but with the transfer of messages from a
+#                   string of arguments to STDERR
+#     warn+exit   - the same as 'echo+exit', but with the transfer of messages
+#                   to the notification system
+#     throw       - the same as 'warn+exit', but with the transfer of messages
+#                   and the call stack to the notification system
+#    <state>      - handled error type, are supported:
+#                     CommandNotFound
+#                     NoSuchFileOrDir
+#                     InvalidVariable
+#                     EmptyVariable
+#                     MissingArgument
+#                     EmptyResult
+#    <argument>   - checked value by type (see <state>)
+#  OUTPUT
+#    see on error ...
+#  EXAMPLE
+#    ## TODO improve tests
+#    err::handler warn on NoSuchFileOrDir /notexist                             #? true
+#    warn on NoSuchFileOrDir /notexist                                          #? true
+#    ECHO on NoSuchFileOrDir /notexist                                          #? true
+#    $(throw on NoSuchFileOrDir /notexist)                                      #? $_bashlyk_iErrorNoSuchFileOrDir
+#    $(EXIT on CommandNotFound notexist)                                        #? $_bashlyk_iErrorCommandNotFound
+#    warn on CommandNotFound notexist                                           #? true
+#  SOURCE
+err::handler() {
+
+  local -a aErrHandler=( $* )
+  local re='^(echo|warn)$|^((echo|warn)[+])?(exit|return)$|^throw$'
+  ## TODO add reAction and reState for safely arguments parsing
+
+  [[ $1 =~ $re ]] && shift || on error throw InvalidArgument "1 - $1"
+  [[ $1 == on  ]] && shift || on error throw InvalidArgument "2 - $1"
+  [[ $1        ]] && shift || on error throw MissingArgument "3 - $1"
+
+  if ! declare -pf err::${aErrHandler[2]} >/dev/null 2>&1; then
+
+    on error throw InvalidArgument "${aErrHandler[2]}"
+
+  fi
+
+  unset aErrHandler[1]
+
+  ## TODO many arguments checking mode
+  err::${aErrHandler[2]} $* || on error ${aErrHandler[@]}
+
+}
+#******
